edac033e8c6105fe47ee8f936fc0e24d19f3be84
afonsir17 <afonsir17@gmail.com>
2020-12-30 19:28:34 -0300

    Refactoring: removing useless code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import { MikroORM } from '@mikro-orm/core';
 import mikroConfig from './mikro-orm.config';
 
-import Post from './entities/Post';
-
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
