84829771ad868d1ca32c1f5f3442171ab457daef
afonsir17 <afonsir17@gmail.com>
2020-12-31 10:28:37 -0300

    Adding Post resolver

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
 import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
 import mikroConfig from './mikro-orm.config';
+import PostResolver from './resolvers/post';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('Server started on localhost:4000');
+  });
 };
 
 main().catch(err => {
