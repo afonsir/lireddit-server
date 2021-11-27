022c498a71a9ed18b7a4c6e043f51f8f321c1734
AfonSir <afonsir17@gmail.com>
2021-11-27 19:37:45 -0300

    Feat: add user entity

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,14 +2,16 @@ import { MikroORM } from '@mikro-orm/core';
 
 import path from 'path';
 import isProd from './constants';
+
 import Post from './entities/Post';
+import User from './entities/User';
 
 export default {
   migrations: {
     path: path.join(__dirname, './migrations'),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: 'lireddit',
   user: 'lireddit',
   password: 'liredditpwd',
